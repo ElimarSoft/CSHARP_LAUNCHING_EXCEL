860f5e768f6b6d61a5283786b501f9d7dc0dec25
Juan Sala <Elimar@jsalaelimar.onmicrosoft.com>
2023-04-22 16:27:35 +0200

    Add project files.

---
+++ WinTest01/ExcelClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WinTest01
+{
+    internal class ExcelClass
+    {
+
+        Excel.Application xlApp;
+        Excel.Workbook wb1;
+        Excel.Worksheet ws1;
+        const string Path1 = @"C:\Users\micro\Desktop\CAD\Excel\";
+        const string File1 = "Trays.xlsx";
+
+        public bool openExcel()
+        {
+            bool result = true;
+
+            try
+            {
+                xlApp = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            }
+            catch (Exception)
+            {
+                xlApp = new Excel.Application();
+            }
+
+            try
+            {
+                wb1 = xlApp.Workbooks[File1];
+                result = false;
+            }
+            catch (Exception)
+            {
+                if (File.Exists(Path1+File1)) wb1 = xlApp.Workbooks.Open(Path1+File1, true, false);
+                else
+                {
+                    wb1 = xlApp.Workbooks.Add();
+                    wb1.SaveAs(Path1+File1);
+                }
+            }
+
+            //ws1 = (Excel.Worksheet)wb1.Worksheets.get_Item(1);
+            ws1 = wb1.Worksheets["Sheet1"];
+
+            return result;
+
+         }   
+
+        public void writeExcel(string Data)
+        {
+            ws1.Cells[1, 1] = Data;
+        }
+
+        public void closeExcel()
+        {
+            wb1.Close(true);
+        }
+
+
+    }
+}
---
+++ WinTest01/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTest01
+{
+    public partial class Form1 : Form
+    {
+        ExcelClass Ex1 = new ExcelClass();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            bool res = Ex1.openExcel();
+            Ex1.writeExcel(this.textBox1.Text);
+            if (res) Ex1.closeExcel();
+
+        }
+    }
+}
